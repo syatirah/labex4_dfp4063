cc358a68c519230e1fab62969f100ef397fe0720
syatirah <amirsyatirah@gmail.com>
2021-09-13 08:20:37 +0800

    try edit index

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<title>Syatirah 18DDT18F1064</title>
+<h2>Terminology</h2>
+<table border ="1">
+
+ <form action="term.php" method="post">
+   
+<tr>
+    <td>Term <br></td>
+   <td>Description  </td>
+</tr>
+<tr>
+  <td><a href="admin_ndex.php">[Add New]</a><br></td>
+  <td></td>
+</tr>
+ 
+    </form>
+
+</table>
+
+</body>
+</html>
+
