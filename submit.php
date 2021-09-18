d0bd22be86db3fcd7d719ed9beb793012d9ad555
syatirah <amirsyatirah@gmail.com>
2021-09-19 06:25:48 +0800

    try edit index

---
+++ submit.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+</head>
+    <body>
+        <center>
+
+<table border ="1">
+<tr>
+    <th>Terms</th>
+   <th>Description</th>
+</tr>
+<tr>
+  <td>[<a href="index.php?add=new">Add New</a>]</td>
+  <td></td>
+</tr>
+<tr>
+  <td>[<a href="index.php?add=new">Add New</a>]</td>
+  <td></td>
+</tr>
+</table>
+</center>
+</body>
+</html>
