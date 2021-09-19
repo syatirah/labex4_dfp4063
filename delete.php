da69adcddb7911476dac928815080bec9ba1152f
syatirah <amirsyatirah@gmail.com>
2021-09-19 15:34:26 +0800

    try edit index

--- delete.php
+++ delete.php
@@ -1,6 +1,8 @@
 <?php
+
 $terms = $_GET['terms'];
 $myfile = 'terms/'.$terms.'.txt';
 unlink($myfile);
 header("location:index.php");
+
 ?>
--- submit.php
+++ submit.php
@@ -1,8 +1,10 @@
     <?php
+
     $terms = $_POST["terms"];
     $description = $_POST["description"];
     $myfile = fopen("terms/$terms.txt" , "w") or die ("Unable to open file!");
     fwrite($myfile, $description);
     fclose($myfile);
     header('location:index.php');
+    
     ?>
